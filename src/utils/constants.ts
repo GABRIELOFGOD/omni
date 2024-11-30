af639e4814777eb31abc4f19a0140956bcfcfddd
GABRIELOFGOD <gabrielayodele2021@gmail.com>
2024-11-30 12:56:01 +0100

    IT HAS ALWAYS BEEN GOD

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,55 +1,55 @@
-import { MdOutlineDashboard } from "react-icons/md";
-import { NavListType } from "./index";
+// import { MdOutlineDashboard } from "react-icons/md";
+// import { NavListType } from "./index";
 
-export const MenuItems: NavListType[] = [
-  {
-    title: "Order List",
-    path: "/dashboard/orders",
-    icon: <MdOutlineDashboard />
-  },
-  {
-    title: "Order List",
-    path: "/dashboard/orders",
-    icon: <MdOutlineDashboard />
-  },
-  {
-    title: "Order List",
-    path: "/dashboard/orders",
-    icon: <MdOutlineDashboard />
-  },
-  {
-    title: "Order List",
-    path: "/dashboard/orders",
-    icon: <MdOutlineDashboard />
-  },
-  {
-    title: "Order List",
-    path: "/dashboard/orders",
-    icon: <MdOutlineDashboard />
-  },
-  {
-    title: "Order List",
-    path: "/dashboard/orders",
-    icon: <MdOutlineDashboard />
-  },
-  {
-    title: "Order List",
-    path: "/dashboard/orders",
-    icon: <MdOutlineDashboard />
-  },
-  {
-    title: "Order List",
-    path: "/dashboard/orders",
-    icon: <MdOutlineDashboard />
-  },
-  {
-    title: "Order List",
-    path: "/dashboard/orders",
-    icon: <MdOutlineDashboard />
-  },
-  {
-    title: "Order List",
-    path: "/dashboard/orders",
-    icon: <MdOutlineDashboard />
-  },
-];
+// export const MenuItems: NavListType[] = [
+//   {
+//     title: "Order List",
+//     path: "/dashboard/orders",
+//     icon: <MdOutlineDashboard />
+//   },
+//   {
+//     title: "Order List",
+//     path: "/dashboard/orders",
+//     icon: <MdOutlineDashboard />
+//   },
+//   {
+//     title: "Order List",
+//     path: "/dashboard/orders",
+//     icon: <MdOutlineDashboard />
+//   },
+//   {
+//     title: "Order List",
+//     path: "/dashboard/orders",
+//     icon: <MdOutlineDashboard />
+//   },
+//   {
+//     title: "Order List",
+//     path: "/dashboard/orders",
+//     icon: <MdOutlineDashboard />
+//   },
+//   {
+//     title: "Order List",
+//     path: "/dashboard/orders",
+//     icon: <MdOutlineDashboard />
+//   },
+//   {
+//     title: "Order List",
+//     path: "/dashboard/orders",
+//     icon: <MdOutlineDashboard />
+//   },
+//   {
+//     title: "Order List",
+//     path: "/dashboard/orders",
+//     icon: <MdOutlineDashboard />
+//   },
+//   {
+//     title: "Order List",
+//     path: "/dashboard/orders",
+//     icon: <MdOutlineDashboard />
+//   },
+//   {
+//     title: "Order List",
+//     path: "/dashboard/orders",
+//     icon: <MdOutlineDashboard />
+//   },
+// ];
